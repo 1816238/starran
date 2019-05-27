8c89613754d28f2d0351daf4cb4f32ffb0295dfe
nitta <hiromu.1999.1227@gmail.com>
2019-05-27 09:48:46 +0900

    敵的追加

---
+++ starrun【PROGRAM】/Project2/EnemyBit.cpp
@@ -0,0 +1,80 @@
+#include <DxLib.h>
+#include "ImageMng.h"
+#include "Enemy.h"
+#include "VECTOR2.h"
+#include "EnemyBit.h"
+
+#define PI  3.1415926535897932384626433832795f
+#define CIRCLE_PI ( PI*2 )
+
+EnemyBit::EnemyBit(VECTOR2 pos, VECTOR2 offset)
+{
+	//Obj::init("image/player_W.png", VECTOR2(40, 64), VECTOR2(1, 1), pos);
+
+	this->pos = pos;
+	this->offset = offset;
+	this->speed = 1;
+	this->objType = TYPE_ENEMY_BIT;
+	EnemyBit::init();
+
+}
+
+EnemyBit::~EnemyBit()
+{
+}
+
+bool EnemyBit::initAnim(void)
+{
+	return false;
+}
+
+bool EnemyBit::init(void)
+{
+	if (objType == TYPE_ENEMY_BIT)
+	{
+		Obj::init("image/boss.png", VECTOR2(43, 45), VECTOR2(5, 1), pos);
+		AddAnim("í èÌ", 1, 0, 4, 10, true);
+		SetAnim("í èÌ");
+	}
+
+
+
+	circle_pos.center_pos = { CENTER_POS_X,CENTER_POS_Y };
+	circle_pos.circle_r = CIRCLE_RANGE;
+	circle_pos.time = 3600;
+	return true;
+}
+
+OBJ_TYPE EnemyBit::CheckObjType(void)
+{
+	return OBJ_TYPE();
+}
+
+bool EnemyBit::CheckDeath(void)
+{
+	return false;
+}
+
+void EnemyBit::SetMove(const GameCtl & controller, weekListObj objList)
+{
+	float x=0;
+	float y=0;
+	SetAnim("í èÌ");
+
+	cnt++;
+
+	tmp_pos_x = circle_pos.center_pos.x + cos(CIRCLE_PI / circle_pos.time*cnt) * circle_pos.circle_r;
+	tmp_pos_y = circle_pos.center_pos.y + sin(CIRCLE_PI / circle_pos.time*cnt) * circle_pos.circle_r;
+/*
+	pos.x = circle_pos.center_pos.x * cos(CIRCLE_PI / circle_pos.time*cnt) * circle_pos.circle_r;
+	pos.y = circle_pos.center_pos.y + cos(CIRCLE_PI / circle_pos.time*cnt) * circle_pos.circle_r;*/
+	
+	SetPos(VECTOR2(tmp_pos_x, tmp_pos_y));
+}
+
+//void EnemyBit::Draw(void)
+//{
+//	SetAnim("í èÌ");
+//	DrawGraph(0, 0, IMAGE_ID("image/boss.png")[0],true);
+//	DrawBox(tmp_pos_x, tmp_pos_y, tmp_pos_x + 100, tmp_pos_y + 100, 0x00ff00, false);
+//}
---
+++ starrun【PROGRAM】/Project2/EnemyBit.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Obj.h"
+
+typedef struct Move_Circle
+{
+	float x;
+	float y;
+};
+
+typedef struct Move_Pos
+{
+	Move_Circle center_pos;	//’†SÀ•W
+	float time;				//üŠú
+	int circle_r;			//‰~‚Ì”¼Œa
+};
+
+class EnemyBit :
+	public Obj
+{
+public:
+	EnemyBit(VECTOR2 pos, VECTOR2 offset);
+	~EnemyBit();
+	bool initAnim(void);
+	bool init(void);
+	OBJ_TYPE CheckObjType(void);
+	bool CheckDeath(void);
+private:
+	void SetMove(const GameCtl &controller, weekListObj objList);		//
+	//void Draw(void);
+	double RotAngle;
+	VECTOR2 offset;
+	int speed;
+	int time;
+	bool deathFlag;
+	/*‰~‰^“®ŒvZ—p*/
+	Move_Pos circle_pos;
+	int cnt;
+	float tmp_pos_x;
+	float tmp_pos_y;
+};
+
