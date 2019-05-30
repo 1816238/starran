8ee6744005e4ab46f455c3684a8b073fa9abb155
nitta <hiromu.1999.1227@gmail.com>
2019-05-30 15:41:49 +0900

    ダメージを受けた際画面が振動する処理

--- starrun【PROGRAM】/Project2/Effect.h
+++ starrun【PROGRAM】/Project2/Effect.h
@@ -11,11 +11,18 @@ public:
 		return s_Instance;
 	}
 	void Shake(void);
-	//void Draw();
+	void Draw();
+	const void SetEffectFlag(bool flag);
+	const bool GetEffectFlag();
+	Effect(Effect& in) {};
+	void operator=(Effect&in) {};
 private:
 	Effect();
 	~Effect();
 	bool init();
 	int effect_image;
+	bool effect_flag;
+	bool shake_flag;
+	int shake_cnt;
 };
 
